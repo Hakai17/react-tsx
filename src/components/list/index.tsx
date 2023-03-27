fb92a0d4ffd36df7ec30b810717d7af6d22eb04c
Hakai17 <miguel.costa.arruda08@aluno.ifce.edu.br>
2023-03-27 15:00:38 -0300

    starting to create dynamic lists

--- src/components/list/index.tsx
+++ src/components/list/index.tsx
@@ -1,11 +1,29 @@
 import React from "react";
 
-function List () {
-    return (
-        <aside>
-
-        </aside>
-    )
+function List() {
+    const tasks = [{
+        task: "React",
+        time: "02:00:00"
+    }, {
+        task: "TypeScript",
+        time: "01:30:00"
+    }, {
+        task: "Docker",
+        time: "01:00:00"
+    }]
+  return (
+    <aside>
+      <h2>Studies of the day</h2>
+      <ul>
+        {tasks.map((item, index) => (
+            <li key={index}>
+                <h3>{item.task}</h3>
+                <span>{item.time}</span>
+            </li>
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default List;
